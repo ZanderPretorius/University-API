202e123b12743736ec5aba8880e89ebd4ebe6d01
ZanderPretorius <zmanpet48@gmail.com>
2022-11-28 13:40:37 +0200

    refined country name checker

--- country_name_check.py
+++ country_name_check.py
@@ -1,4 +1,8 @@
 def check_country_name(cd_name):
+
+    us_states = ["Alaska", "Alabama", "Arkansas", "American Samoa", "Arizona", "California", "Colorado", "Connecticut", "District of Columbia", "Delaware", "Florida", "Georgia", "Guam", "Hawaii", "Iowa", "Idaho", "Illinois", "Indiana", "Kansas", "Kentucky", "Louisiana", "Massachusetts", "Maryland", "Maine", "Michigan", "Minnesota", "Missouri", "Mississippi",
+                 "Montana", "North Carolina", "North Dakota", "Nebraska", "New Hampshire", "New Jersey", "New Mexico", "Nevada", "New York", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Puerto Rico", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Virginia", "Virgin Islands", "Vermont", "Washington", "Wisconsin", "West Virginia", "Wyoming"]
+
     res_name = ""
     c_name = cd_name.lower()
     if c_name == "usa":
@@ -13,9 +17,20 @@ def check_country_name(cd_name):
     elif c_name == "the netherlands":
         res_name = "netherlands"
 
-    elif c_name == "uk" or c_name == "england" or c_name == "wales" or c_name == "scotland" or c_name=="the united kingdom" or c_name=="northern ireland":
+    elif c_name == "uk" or c_name == "england" or c_name == "wales" or c_name == "scotland" or c_name == "the united kingdom" or c_name == "northern ireland":
         res_name = "united kingdom"
 
+    elif c_name == "somaliland":
+        res_name = "somalia"
+
+    elif c_name == "eswatini":
+        res_name = "swaziland"
+
+    elif c_name == "kingdom of saudi arabia":
+        res_name = "saudi arabia"
+
+    elif c_name.title() in us_states:
+        res_name = "united states"
     else:
         res_name = c_name
 
